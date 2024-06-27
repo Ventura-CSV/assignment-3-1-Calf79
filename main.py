2e5d0bf2cb9a28fe1f1a8fcf3f11427a182a4829
Calf79 <camerino_alfaro2@my.vcccd.edu>
2024-06-26 23:18:16 -0700

    3-1-Camerino A

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ def main():
     Code Your Program here
     ########################################
     """
-
+    print(number1,number2,number3)
+    min_value= number1 
+    min_value= number2 if number2<= min_value else min_value
+    min_value= number3 if number3<= min_value else min_value
+    print(min_value)
+   
     ########################################
     # Do not delete the return statement
     ########################################
